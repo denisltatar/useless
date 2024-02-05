7db5c5d66a0f5bbcf8252d8ca843b15a17aac4ba
Denis Tatar <denistatar@Deniss-MacBook-Pro.local>
2024-02-05 17:30:35 -0500

    first commit

---
+++ useless Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  useless Watch App
+//
+//  Created by Denis Tatar on 2024-02-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Jamiepoo!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ useless Watch App/uselessApp.swift
@@ -0,0 +1,17 @@
+//
+//  uselessApp.swift
+//  useless Watch App
+//
+//  Created by Denis Tatar on 2024-02-05.
+//
+
+import SwiftUI
+
+@main
+struct useless_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
