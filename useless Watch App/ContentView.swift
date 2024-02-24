81ea5a112503ca02ef5715ac658dc0c36919ad83
Denis Tatar <denistatar@Deniss-MacBook-Pro.local>
2024-02-24 16:06:02 -0500

    added first button

--- useless Watch App/ContentView.swift
+++ useless Watch App/ContentView.swift
@@ -10,13 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image("useless")
-                .imageScale(.small)
+//            Image("useless")
+//                .imageScale(.small).onTapGesture {
+//                    print("tapped!")
+//                }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+            // Creating a simple string variable to be replace upon a button being tapped
             Text("Hello, Jamiepoo!").font(.custom("Cochin", fixedSize: 10))
-            
+            Button (action: {
+                // Execute action
+            }, label: { Text("Generate")})
+
         }
         .padding()
     }
